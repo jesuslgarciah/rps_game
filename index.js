235c738153e06e57be7b7f94f78995173a8981fb
jesusleonardo.garcia <jesusleonardo.garcia@bbva.com>
2020-08-19 22:49:24 -0500

    modificar el incremento por medio de clousure, agregar estilos, arreglar algo de maquetado y establecer el footer con mis redes sociales

--- index.js
+++ index.js
@@ -1,26 +1,110 @@
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissor = document.getElementById("scissor")
+const finish = document.getElementById("finish")
+const restart = document.getElementById("restart")
+const image_gamer = document.getElementById('gamer')
+const image_computer = document.getElementById('computer')
+const winner = document.getElementById('winner')
+const countGamer = document.getElementById('count-gamer')
+const countComputer = document.getElementById('count-computer')
 
-let gamer = 0
-let computer = 0
+const rock_image = `<img id="rock" src="https://img.icons8.com/color/48/000000/hand-rock.png"/>`
+const paper_image = `<img id="paper" src="https://img.icons8.com/color/48/000000/hand.png"/>`
+const scissor_image = `<img id="scissor" src="https://img.icons8.com/color/48/000000/hand-scissors.png"/>`
+
+let counterGamer = makeCounter(0)
+let counterComputer = makeCounter(0)
+
+init()
 
 rock.addEventListener("click", () => {
-    main("rock")
+    image_gamer.innerHTML = rock_image
+    game("rock")
 })
 
 paper.addEventListener("click", () => {
-    main("paper")
+    image_gamer.innerHTML = paper_image
+    game("paper")
 })
 
 scissor.addEventListener("click", () => {
-    main("scissor")
+    image_gamer.innerHTML = scissor_image
+    game("scissor")
 })
 
-function main(option){
-    console.log(`You: ${option}`)
+finish.addEventListener("click", () => {
+    endGame()
+})
+
+restart.addEventListener("click", () => {
+    endGame()
+})
+
+function init(){
+    image_gamer.classList.add('hidden')
+    image_computer.classList.add('hidden')
+    winner.classList.add('hidden')
+    counterGamer.restart()
+    counterComputer.restart()
+    countGamer.innerHTML = counterGamer.getCount()
+    countComputer.innerHTML = counterComputer.getCount()
+}
+
+function makeCounter(n){
+    let count = n
+    return{
+        increase: function(){
+            return ++count
+        },
+        getCount: function(){
+            return count
+        },
+        restart: function(){
+            count = 0
+        }
+    }
+}
+
+function printMessageWinner(message){
+    winner.innerHTML = `<p>¡${message}!</p>`
+}
+
+function randomHand(){
+    let random = Math.ceil((Math.random()) * 3)
+    switch(random){
+        case 1:
+            image_computer.innerHTML = rock_image
+            return "rock"
+        case 2:
+            image_computer.innerHTML = paper_image
+            return "paper"
+        case 3:
+            image_computer.innerHTML = scissor_image
+            return "scissor"
+    }
+}
+
+
+
+function increase(win) {
+    if (win == "gamer"){
+        countGamer.innerHTML = counterGamer.increase()
+    }
+    else if (win == "computer"){
+        countComputer.innerHTML = counterComputer.increase()
+    }
+}
+
+function endGame(){
+    init()
+}
+
+function game(option){
+    image_gamer.classList.remove('hidden')
+    image_computer.classList.remove('hidden')
+    winner.classList.remove('hidden')
     let computerHand = randomHand()
-    console.log(`Computer: ${computerHand}`)
     if (option === "rock"){
         switch (computerHand){
             case "rock":
@@ -63,40 +147,5 @@ function main(option){
                 printMessageWinner("No one wins")
                 break
         }
-    }
-}
-
-function printMessageWinner(message){
-    const winner = document.getElementById('winner')
-    winner.innerHTML = `<label>¡${message}!</label>`
-}
-
-function randomHand(){
-    const image_computer = document.getElementById('computer')
-    const rock = `<img id="rock" src="https://img.icons8.com/color/48/000000/hand-rock.png"/>`
-    const paper = `<img id="paper" src="https://img.icons8.com/color/48/000000/hand.png"/>`
-    const scissor = `<img id="scissor" src="https://img.icons8.com/color/48/000000/hand-scissors.png"/>`
-    let random = Math.ceil((Math.random()) * 3)
-    switch(random){
-        case 1:
-            image_computer.innerHTML = (rock)
-            return "rock"
-        case 2:
-            image_computer.innerHTML = (paper)
-            return "paper"
-        case 3:
-            image_computer.innerHTML = (scissor)
-            return "scissor"
-    }
-}
-
-function increase(winner) {
-    if (winner == "gamer"){
-        const countGamer = document.getElementById('count-gamer')
-        countGamer.innerHTML = ++gamer
-    }
-    else if (winner == "computer"){
-        const countGamer = document.getElementById('count-computer')
-        countGamer.innerHTML = ++computer
     }
 }
